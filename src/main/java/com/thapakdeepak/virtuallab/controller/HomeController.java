beb75f174d3193a9b221c465b1365a9ded79a399
Deepak Sharma <112003902+deepak948270@users.noreply.github.com>
2024-01-06 22:17:39 +0530

    first commit

---
+++ src/main/java/com/thapakdeepak/virtuallab/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.thapakdeepak.virtuallab.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/andgate")
+    public String andGate(){
+        return "andGate";
+    }
+    
+}
